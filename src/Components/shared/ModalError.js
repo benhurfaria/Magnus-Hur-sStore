3649ed08728d4b2bfeaf7222349d67a27459cb24
Francisca <fraan928@gmail.com>
2021-11-11 10:22:14 -0300

    Limpando codigo

--- src/Components/shared/ModalError.js
+++ src/Components/shared/ModalError.js
@@ -3,6 +3,7 @@ import { Button } from "../styles/ButtonStyle";
 
 import { IoAlertCircleSharp } from 'react-icons/io5'
 import { IconContext } from "react-icons/lib";
+import { Message, Modal } from "../styles/ModalStyle";
 
 export default function ModalError({ message, setModal }) {
     return (
@@ -20,46 +21,3 @@ export default function ModalError({ message, setModal }) {
         </Modal>
     );
 }
-
-const Modal = styled.div`
-    width: 100vw;
-    height: 100vh;
-
-    background-color: rgba(238, 238, 238, 0.6);
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    position: fixed;
-    top: 0;
-    left: 0;
-
-    z-index: 10;
-`;
-
-const Message = styled.div`
-    width: 90vw;
-    height: 35vh;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-
-    background-color: #ffffff;   
-    border: 1px solid #283D3B;
-    border-radius: 10px;
-    
-    box-shadow: 8px 8px 8px rgba(0, 0, 0, 0.15);
-
-    transition: all 0.4s ease 0,3s;
-
-    h1 {
-        color: #283D3B;
-        font-size: 26px;
-        text-align: center;
-
-        padding: 20px 0;
-    }
-`;
